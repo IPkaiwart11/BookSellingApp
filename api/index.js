aac6288a7ab9858a5a5a2d3ad7858e9da5a9afe1
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-05-13 20:02:36 +0530

    solve problems2

--- api/index.js
+++ api/index.js
@@ -1,47 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const userRoute = require("./routes/user");
-// const authRoute = require("./routes/auth");
-// const productRoute = require("./routes/product");
-// const cartRoute = require("./routes/cart");
-// const orderRoute = require("./routes/order");
-// const cors = require("cors");
-
-
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => console.log("DB Connection Successfull!"))
-//   .catch((err) => {
-//     console.log("mongodb connection error", err);
-//   });
-
-// // app.use(cors());
-// app.use(cors({
-//   origin: 'https://book-selling-app.vercel.app',
-//   // origin: 'https://localhost:3000',
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE' , 'HEAD' , 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'token'], 
-// }));
-
-
-// app.use(express.json());
-
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/products", productRoute);
-// app.use("/api/carts", cartRoute);
-// app.use("/api/orders", orderRoute);
-
-// const port = process.env.PORT || 5000
-// app.listen(port, () => {
-//   console.log("Backend server is running!");
-// });
-
-
 
 const express = require("express");
 const app = express();
@@ -56,7 +12,7 @@ const orderRoute = require("./routes/order");
 const cors = require("cors");
 console.log("MongoDB Connection URL:", process.env.MONGO_URL);
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB Connection Successful!"))
   .catch((err) => {
     console.error("MongoDB connection error", err);
