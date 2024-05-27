a175497f2cc995817a03eed5ff392d619fb561fb
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-05-28 00:52:58 +0530

    adding order paymnet method

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -138,7 +138,7 @@ export default function Payment() {
         // status: ,
       }
       try{
-        const response = await fetch(`https://booksellingapp.onrender.com/api/orders`,{
+        const response = await fetch("https://booksellingapp.onrender.com/api/orders",{
         // const response = await fetch("http://localhost:5000/api/orders", {
           method: "POST",
           headers: {
