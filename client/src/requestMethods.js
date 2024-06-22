cfde71ec800ca590823125f6755db51135cbcca4
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-06-22 17:20:38 +0530

    initial commit2

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://booksellingapp.onrender.com/api";
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://book-selling-app-ob4s.vercel.app/api";
+// const BASE_URL = "http://localhost:5000/api";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
--- client/src/url.js
+++ client/src/url.js
@@ -1,2 +1,2 @@
 //  export const BASE_URL = 'http://localhost:5000/api'
- export const BASE_URL = 'https://booksellingapp.onrender.com/api'
+ export const BASE_URL = 'https://book-selling-app-ob4s.vercel.app/api'
